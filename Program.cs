23436f30cf0424b91a721a4bcf9bf280b8692976
Felix-1893 <felix.durst@fleigro.onmicrosoft.com>
2025-11-20 14:48:58 +0100

    Fix .NET 8.0 compatibility - remove Assets references

--- Program.cs
+++ Program.cs
@@ -24,13 +24,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
 // HTTPS-Umleitung für Netzwerkzugriff deaktiviert
 // app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
